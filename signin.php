737f395b799e13f316a0f3823438b1da2c3ec566
KairyVu <kairyvd167@gmail.com>
2023-06-02 18:25:11 +0700

    Delete signin.php

--- signin.php
+++
@@ -1,9 +0,0 @@
-<?php
-        //Makes DB connection
-        $servername = "sql1.njit.edu";
-        $username = "kv353";
-        $password = "Kv123456789!";
-        $dbname = "kv353";
-        $con = mysqli_connect($servername,$username,$password,$dbname);
-
-?>
