820ca469c653c5737f036393ca9d9763485a5c4b
KairyVu <kairyvd167@gmail.com>
2023-06-02 19:01:36 +0700

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,8 @@
+<?php
+        //Makes DB connection
+        $servername = "sql1.njit.edu";
+        $username = "kv353";
+        $password = "Kv123456789!";
+        $dbname = "kv353";
+        $con = mysqli_connect($servername,$username,$password,$dbname);
+?>
